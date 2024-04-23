25ed9cb3c2e9efcb8098f09eb182ff12e4f99c3b
acanois <david.richter42@gmail.com>
2024-04-23 15:34:37 -0500

    add synth initialization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,12 @@ import Image from "next/image";
 import * as Tone from 'tone'
 
 import { useEffect } from "react";
+import { BubbleSynth } from "./audio/instruments/bubble-synth";
 
 export default function Home() {
   useEffect(() => {
     Tone.start()
+    const synth = new BubbleSynth()
   })
   return (
     <main className="flex min-h-screen flex-col items-center">
